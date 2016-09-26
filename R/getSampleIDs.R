0b7199663a79125d2195ccc96b9c7a962ea215a3
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-09-26 08:38:32 +0100

    !

--- R/getSampleIDs.R
+++ R/getSampleIDs.R
@@ -8,6 +8,7 @@
 #' @examples
 #' SampleIDs = getSampldIDs("otu_table.txt")
 getSampldIDs <- function (otu_table) {
+  print("reading...")
   header <- scan(otu_table, skip = 1, nlines = 1, what = character(), sep = "\t")
   header = header[2:(length(header)-1)]
   header
