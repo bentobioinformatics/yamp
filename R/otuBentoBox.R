22609009645f9f37fe5a0785190c32142dd43136
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-09-26 14:44:16 +0100

    !

--- R/otuBentoBox.R
+++ R/otuBentoBox.R
@@ -11,6 +11,7 @@ setClass("otuBentoBox",
                    taxonomy = "data.frame")
 )
 
+
 #' Constructor method of otuBentoBox Class.
 #'
 #' @name otuBentoBox
@@ -39,6 +40,27 @@ setMethod(f = "initialize",
             library(tidyr)
             taxonomy = separate(taxonomy, taxonomy , into = c("k", "p", "c", "o", "f", "g", "s"), sep = ";", extra = "drop", fill = "right")
 
+            # Convert NA to unclassified
+            taxonomy$p[is.na(taxonomy$p)] = "p__"
+            taxonomy$c[is.na(taxonomy$c)] = "c__"
+            taxonomy$o[is.na(taxonomy$o)] = "o__"
+            taxonomy$f[is.na(taxonomy$f)] = "f__"
+            taxonomy$g[is.na(taxonomy$g)] = "g__"
+            taxonomy$s[is.na(taxonomy$s)] = "s__"
+
+            # Check integrity
+            otutable.numberofsamples = ncol(otutable)
+            otutable.numberofotus = nrow(otutable)
+            taxonomy.numberofotus = nrow(taxonomy)
+            metadata.numberofsamples = nrow(metadata)
+
+            cat(paste("Numner of samples: ", otutable.numberofsamples, "\n"))
+            cat(paste("Numner of OTUs:    ", otutable.numberofotus, "\n"))
+
+            if (otutable.numberofsamples != metadata.numberofsamples) stop("The number of samples don't match.")
+            if (otutable.numberofotus != taxonomy.numberofotus) stop("The number of OTUs don't match.")
+
+            # Bung them
             .Object@otutable = otutable
             .Object@metadata = metadata
             .Object@taxonomy = taxonomy
@@ -49,7 +71,7 @@ setMethod(f = "initialize",
 )
 
 
-#' Wrapper function Sensor.
+#' Wrapper function otuBentoBox.
 #'
 #' @name otuBentoBox
 #' @rdname otuBentoBox-class
@@ -57,6 +79,53 @@ setMethod(f = "initialize",
 otuBentoBox <- function (otutable_filename, metadata_filename) {
   new("otuBentoBox", otutable_filename, metadata_filename)
 }
+
+
+# Test
+# dataFile_otutable = c("~/Library/Mobile\ Documents/com~apple~CloudDocs/Project/ThamesSampling/04_Bioinformatics/ThamesWBS/16S/otu_table.txt")
+# dataFile_metadata = c("~/Library/Mobile\ Documents/com~apple~CloudDocs/Project/ThamesSampling/05_Analysis/ThamesWBS/metadata_combined.txt")
+# otuBentoBox_16S = otuBentoBox(dataFile_otutable, dataFile_metadata)
+
+
+#' Remove taxa from an OTU Bento Box
+#'
+#' @name otuBentoBox
+#' @rdname otuBentoBox
+#' @param bentoBox Your Bento Box
+#' @param classificationLevel Your classificationLevel
+#' @param nameToRemove Names to remove
+#' @export
+#' @examples
+#' aNewBentoBox = removeTaxaFromBentoBox(aBentoBox, "k", "k__Bacteria")
+removeTaxaFromBentoBox <- function (bentoBox, classificationLevel, nameToRemove) {
+  OTUsToExclude = rownames(bentoBox@taxonomy[ bentoBox@taxonomy[[classificationLevel]] == nameToRemove, ])
+  print(OTUsToExclude)
+  bentoBox@otutable = bentoBox@otutable[!rownames(bentoBox@otutable) %in% OTUsToExclude, ]
+  bentoBox@taxonomy = bentoBox@taxonomy[!rownames(bentoBox@taxonomy) %in% OTUsToExclude, ]
+  return(bentoBox)
+}
+
+
+#' Retain taxa from an OTU Bento Box
+#'
+#' @name otuBentoBox
+#' @rdname otuBentoBox
+#' @param bentoBox Your Bento Box
+#' @param classificationLevel Your classificationLevel
+#' @param nameToRetain Names to remove
+#' @export
+#' @examples
+#' aNewBentoBox = retainTaxaFromBentoBox(aBentoBox, "k", "k__Bacteria")
+retainTaxaFromBentoBox <- function (bentoBox, classificationLevel, nameToRemove) {
+  OTUsToExclude = rownames(bentoBox@taxonomy[ bentoBox@taxonomy[[classificationLevel]] == nameToRemove, ])
+  print(OTUsToExclude)
+  bentoBox@otutable = bentoBox@otutable[rownames(bentoBox@otutable) %in% OTUsToExclude, ]
+  bentoBox@taxonomy = bentoBox@taxonomy[rownames(bentoBox@taxonomy) %in% OTUsToExclude, ]
+  return(bentoBox)
+}
+
+# otuBentoBox_16S_2 = removeTaxaFromBentoBox(otuBentoBox_16S, c, "c__Chloroplast")
+
 
 
 
