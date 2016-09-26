491857cc629a1dd9d475534148e656c85c7fa348
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-09-26 11:12:11 +0100

    !

--- R/getSampleNames.R
+++ R/getSampleNames.R
@@ -1,14 +1,14 @@
 #' getSampleNames
 #'
-#' This function retrieves sample names from the OTU table. The OTU table must be derived from biom.
-#' @param otu_table OTU Table file
+#' This function retrieves sample names from the OTU table.
+#' @param file the name of the OTU table file which the data are to be read from. The OTU table must have derived from biom.
 #' @keywords OTU sample names
 #' @export
 #' @examples
 #' sampleNames = getSampleNames("otu_table.txt")
-getSampleNames <- function (.f_otu_table) {
+getSampleNames <- function (file) {
   print("reading...")
-  header = scan(.f_otu_table, skip = 1, nlines = 1, what = character(), sep = "\t")
+  header = scan(file, skip = 1, nlines = 1, what = character(), sep = "\t")
   header = header[2:(length(header)-1)]
   header
 }
