cb65ad50a9e69e4fda4d2174a93b872dc5968c04
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-09-26 08:33:09 +0100

    getSampleIDs

--- R/getSampleIDs.R
+++ R/getSampleIDs.R
@@ -1,8 +1,11 @@
 #' getSampldIDs
 #'
-#' @name getSampldIDs
-#' @rdname getSampldIDs
+#' This function retrieves Sample IDs from the OTU table.
+#' @param otu_table Your OTU Table.
+#' @keywords OTU sampleIDs
 #' @export
+#' @examples
+#' SampleIDs = getSampldIDs("otu_table.txt")
 getSampldIDs <- function (otutable_filename) {
   header <- scan(dataFile_otu_table, skip = 1, nlines = 1, what = character(), sep = "\t")
   header = header[2:(length(header)-1)]
