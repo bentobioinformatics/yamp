08e4da60e06a310c09f2b79a2421ee9aeef880dc
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-10-16 18:36:16 +0100

    !

--- R/otuBentoBox.R
+++ R/otuBentoBox.R
@@ -19,22 +19,22 @@ setMethod(f = "initialize",
           signature = "otuBentoBox",
           definition = function(.Object, otutable, metadata, taxonomy)  {
 
-            print("Checking integrity...")
-
-            # Check integrity
-            otutable.numberofsamples = ncol(otutable)
-            otutable.numberofotus = nrow(otutable)
-            taxonomy.numberofotus = nrow(taxonomy)
-            metadata.numberofsamples = nrow(metadata)
-
-            # Output
-            cat(paste("[otutable] Number of OTUs   :", dim(otutable)[1], "\n"))
-            cat(paste("           Number of samples:", dim(otutable)[2], "\n"))
-            cat(paste("[metadata] Number of samples:", dim(metadata)[1], "\n"))
-            cat(paste("[taxonomy] Number of OTUs   :", dim(taxonomy)[1], "\n"))
-
-            if (otutable.numberofsamples != metadata.numberofsamples) stop("The number of samples don't match.")
-            if (otutable.numberofotus != taxonomy.numberofotus) stop("The number of OTUs don't match.")
+            # print("Checking integrity...")
+            #
+            # # Check integrity
+            # otutable.numberofsamples = ncol(otutable)
+            # otutable.numberofotus = nrow(otutable)
+            # taxonomy.numberofotus = nrow(taxonomy)
+            # metadata.numberofsamples = nrow(metadata)
+            #
+            # # Output
+            # cat(paste("[otutable] Number of OTUs   :", dim(otutable)[1], "\n"))
+            # cat(paste("           Number of samples:", dim(otutable)[2], "\n"))
+            # cat(paste("[metadata] Number of samples:", dim(metadata)[1], "\n"))
+            # cat(paste("[taxonomy] Number of OTUs   :", dim(taxonomy)[1], "\n"))
+            #
+            # if (otutable.numberofsamples != metadata.numberofsamples) stop("The number of samples don't match.")
+            # if (otutable.numberofotus != taxonomy.numberofotus) stop("The number of OTUs don't match.")
 
             # Bung them
             .Object@otutable = otutable
