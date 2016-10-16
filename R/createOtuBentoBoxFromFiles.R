64c5e1e94bcb01a8ad2d63cca96b37aa8c5edbe9
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-10-16 18:50:52 +0100

    !

--- R/createOtuBentoBoxFromFiles.R
+++ R/createOtuBentoBoxFromFiles.R
@@ -43,20 +43,20 @@ createOtuBentoBoxFromFiles <- function (f_otutable, f_metadata) {
   taxonomy$g[is.na(taxonomy$g)] = "g__"
   taxonomy$s[is.na(taxonomy$s)] = "s__"
 
-  # Check integrity
-  otutable.numberofsamples = ncol(otutable)
-  otutable.numberofotus = nrow(otutable)
-  taxonomy.numberofotus = nrow(taxonomy)
-  metadata.numberofsamples = nrow(metadata)
-
-  # Output
-  cat(paste("[otutable] Number of OTUs   :", dim(otutable)[1], "\n"))
-  cat(paste("           Number of samples:", dim(otutable)[2], "\n"))
-  cat(paste("[metadata] Number of samples:", dim(metadata)[1], "\n"))
-  cat(paste("[taxonomy] Number of OTUs   :", dim(taxonomy)[1], "\n"))
-
-  if (otutable.numberofsamples != metadata.numberofsamples) stop("The number of samples don't match.")
-  if (otutable.numberofotus != taxonomy.numberofotus) stop("The number of OTUs don't match.")
+  # # Check integrity
+  # otutable.numberofsamples = ncol(otutable)
+  # otutable.numberofotus = nrow(otutable)
+  # taxonomy.numberofotus = nrow(taxonomy)
+  # metadata.numberofsamples = nrow(metadata)
+  #
+  # # Output
+  # cat(paste("[otutable] Number of OTUs   :", dim(otutable)[1], "\n"))
+  # cat(paste("           Number of samples:", dim(otutable)[2], "\n"))
+  # cat(paste("[metadata] Number of samples:", dim(metadata)[1], "\n"))
+  # cat(paste("[taxonomy] Number of OTUs   :", dim(taxonomy)[1], "\n"))
+  #
+  # if (otutable.numberofsamples != metadata.numberofsamples) stop("The number of samples don't match.")
+  # if (otutable.numberofotus != taxonomy.numberofotus) stop("The number of OTUs don't match.")
 
   # Bung them
   newOtuBentoBox = otuBentoBox(otutable, metadata, taxonomy)
