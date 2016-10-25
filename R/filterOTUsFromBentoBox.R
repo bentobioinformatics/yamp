0239b90a146f7a2c565efa09ae394d0108f917dc
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-10-25 21:03:09 +0100

    !

--- R/filterOTUsFromBentoBox.R
+++ R/filterOTUsFromBentoBox.R
@@ -14,11 +14,11 @@ filterOTUsFromBentoBox <- function (bentoBox, OTUsToRemove, type = "remove") {
 
   # Filter
   if (type == "remove") {
-    .bentoBox@otutable = bentoBox@otutable[!rownames(.bentoBox@otutable) %in% OTUsToRemove, ]
-    .bentoBox@taxonomy = bentoBox@taxonomy[!rownames(.bentoBox@otutable) %in% OTUsToRemove, ]
+    .bentoBox@otutable = bentoBox@otutable[!rownames(bentoBox@otutable) %in% OTUsToRemove, ]
+    .bentoBox@taxonomy = bentoBox@taxonomy[!rownames(bentoBox@taxonomy) %in% OTUsToRemove, ]
   } else if (type == "retain") {
-    .bentoBox@otutable = bentoBox@otutable[rownames(.bentoBox@otutable) %in% OTUsToRemove, ]
-    .bentoBox@taxonomy = bentoBox@taxonomy[rownames(.bentoBox@otutable) %in% OTUsToRemove, ]
+    .bentoBox@otutable = bentoBox@otutable[rownames(bentoBox@otutable) %in% OTUsToRemove, ]
+    .bentoBox@taxonomy = bentoBox@taxonomy[rownames(bentoBox@taxonomy) %in% OTUsToRemove, ]
   } else {
     stop("Type needs to be either remove or retain")
   }
