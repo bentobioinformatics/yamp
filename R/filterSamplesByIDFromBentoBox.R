62dded07fe206f5b64a18ab7f5410db287e47637
Bento Bioinformatics <bentobioinformatics@gmail.com>
2016-11-24 11:19:16 +0000

    !

--- R/filterSamplesByIDFromBentoBox.R
+++ R/filterSamplesByIDFromBentoBox.R
@@ -19,26 +19,26 @@ filterSamplesByIDFromBentoBox <- function (bentoBox, samplesToRemove, type = "re
     .bentoBox@otutable = bentoBox@otutable[, !colnames(bentoBox@otutable) %in% samplesToRemove, drop = FALSE]
     .bentoBox@metadata = bentoBox@metadata[!colnames(bentoBox@otutable) %in% samplesToRemove, , drop = FALSE]
   } else if (type == "retain") {
-    .bentoBox@otutable = bentoBox@otutable[, which(colnames(bentoBox@otutable) %in% samplesToRemove), drop = FALSE]
-    .bentoBox@metadata = bentoBox@metadata[which(colnames(bentoBox@otutable) %in% samplesToRemove), , drop = FALSE]
+    .bentoBox@otutable = bentoBox@otutable[, colnames(bentoBox@otutable) %in% samplesToRemove, drop = FALSE]
+    .bentoBox@metadata = bentoBox@metadata[colnames(bentoBox@otutable) %in% samplesToRemove, , drop = FALSE]
   } else {
     stop("Type needs to be either remove or retain")
   }
 
-  # # Remove zero-sum OTUs
-  # .zeroSumOTUs = which(!apply(.bentoBox@otutable, 1, FUN = function(x){ sum(x) == 0 }))
-  # .bentoBox@otutable = .bentoBox@otutable[.zeroSumOTUs, ]
-  # .bentoBox@taxonomy = .bentoBox@taxonomy[.zeroSumOTUs, ]
-  #
-  # # Clean
-  # .bentoBox@otutable = droplevels(.bentoBox@otutable)
-  # .bentoBox@metadata = droplevels(.bentoBox@metadata)
-  #
-  # # Output
-  # cat(paste("[otutable] Number of OTUs   :", dim(.bentoBox@otutable)[1], "\n"))
-  # cat(paste("           Number of samples:", dim(.bentoBox@otutable)[2], "\n"))
-  # cat(paste("[metadata] Number of samples:", dim(.bentoBox@metadata)[1], "\n"))
-  # cat(paste("[taxonomy] Number of OTUs   :", dim(.bentoBox@taxonomy)[1], "\n"))
+  # Remove zero-sum OTUs
+  .zeroSumOTUs = which(!apply(.bentoBox@otutable, 1, FUN = function(x){ sum(x) == 0 }))
+  .bentoBox@otutable = .bentoBox@otutable[.zeroSumOTUs, , drop = FALSE]
+  .bentoBox@taxonomy = .bentoBox@taxonomy[.zeroSumOTUs, , drop = FALSE]
+
+  # Clean
+  .bentoBox@otutable = droplevels(.bentoBox@otutable)
+  .bentoBox@metadata = droplevels(.bentoBox@metadata)
+
+  # Output
+  cat(paste("[otutable] Number of OTUs   :", dim(.bentoBox@otutable)[1], "\n"))
+  cat(paste("           Number of samples:", dim(.bentoBox@otutable)[2], "\n"))
+  cat(paste("[metadata] Number of samples:", dim(.bentoBox@metadata)[1], "\n"))
+  cat(paste("[taxonomy] Number of OTUs   :", dim(.bentoBox@taxonomy)[1], "\n"))
   return(.bentoBox)
 
 }
